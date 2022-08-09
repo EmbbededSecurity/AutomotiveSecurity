f5acdaa42154d6aae200693ec79b7f09035889f2
MasonJeon <mason@korea.ac.kr>
2022-08-09 12:38:18 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -65,3 +65,24 @@ def Signal_Print(BUS_interface, PCAN_BAUD, ID, SignalStart, SignalEnd):
                 i = 0
                 data = ("{0}\t {1}\t {2}\t".format(time, hex(R_msg.ID), R_msg.LEN))
 
+                datafull = []
+                if ID == hex(R_msg.ID):
+                    for i in range(0, R_msg.LEN):
+                        data1 = str(bin(R_msg.DATA[i])[2:])
+                        if len(data1) == 1:
+                            data1 = str("000" + str(data1))
+                        if len(data1) == 2:
+                            data1 = str("00" + str(data1))
+                        if len(data1) == 3:
+                            data1 = str("0" + str(data1))
+                        datafull.append(data1)
+                        # print(data_field_tmp)
+                    datafull = ''.join(datafull)
+
+                    signalvalue = []
+                    for i in range(SignalEnd):
+                        signalvalue.append(datafull[SignalStart + i])
+                    signalvalue = ''.join(signalvalue)
+                    signalvalue = int(signalvalue, 2)
+
+                    print(signalvalue)
